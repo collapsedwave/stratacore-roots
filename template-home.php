aae270e7dcf2de0b323c58f4c81e7f347dc39a48
collapsedwave <josh@collapsedwave.com>
2014-03-14 21:55:41 -0400

    home template custom fields

--- template-home.php
+++ template-home.php
@@ -4,74 +4,66 @@ Template Name: Home Template
 */
 ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
-
-<div class="container-full home-hero">
+<div class="container-full clearfix home-hero">
 	<div class="container col-sm-12 row">
 		<div class="col-sm-8 col-sm-offset-2">
-			<h1>site header</h1>
-			<p>
-			</p>
+			<h1><?php the_field('hero_title'); ?></h1>
+			<p><?php the_field('hero_copy'); ?></p>
 			<button class="btn btn-primary">Contact Us</button>
 		</div>
 	</div>
 </div>
 
-<div class="container-full home-services">
+<div class="container-full clearfix home-services">
 	<div class="container">
 		<div class="col-sm-4">
-			<h3>service header</h3>
-			<p>
-				
-			</p>
+			<h3><?php the_field('service_title_1'); ?></h3>
+			<p><?php the_field('service_copy_1'); ?></p>
 		</div>
 		<div class="col-sm-4">
-			<h3>service header</h3>
-			<p>
-				
-			</p>
+			<h3><?php the_field('service_title_2'); ?></h3>
+			<p><?php the_field('service_copy_2'); ?></p>
 		</div>
 		<div class="col-sm-4">
-			<h3>service header</h3>
-			<p>
-				
-			</p>
+			<h3><?php the_field('service_title_3'); ?></h3>
+			<p><?php the_field('service_copy_3'); ?></p>
 		</div>
 	</div>
 </div>
 
-<div class="container-full home-usp">
+<div class="container-full clearfix home-usp">
 	<div class="container">
 		<div class="col-sm-8 col-sm-offset-2">
-			<h2>usp header</h2>
-			<p>
-				
-			</p>
+			<h2><?php the_field('proposition_title'); ?></h2>
+			<p><?php the_field('proposition_copy'); ?></p>
 		</div>
 	</div>
 </div>
 
-<div class="container-full home-team">
+<div class="container-full clearfix home-team">
 	<div class="container">
 		<div class="col-sm-6">
-			<h3>team header</h3>
-			<p>
-				
-			</p>
+			<h3><?php the_field('team_title'); ?></h3>
+			<p><?php the_field('team_copy_1'); ?></p>
 		</div>
 		<div class="col-sm-6">
-			<p>
-				
-			</p>
+			<p><?php the_field('team_copy_2'); ?></p>
 		</div>
 	</div>
 </div>
 
-<div class="container-full home-contact">
+<div class="container-full clearfix home-contact">
 	<div class="container">
 		<div class="col-sm-6">
+		<p>
+			25 Broadway<br>
+			5th Floor<br>
+			New York, NY, 10025
+		</p>
 		</div>
 		<div class="col-sm-6">
+			<span class="phone">212.123.4567</span>
+			<a href="mailto:contact@stratacore.net">contact@stratacore.net</a>
 		</div>
 	</div>
 </div>
