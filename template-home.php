1d139b8f338e76faa3aeb7d076c65bdfbd6a6949
collapsedwave <josh@collapsedwave.com>
2014-03-14 21:01:30 -0400

    initial home template

---
+++ template-home.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Template Name: Home Template
+*/
+?>
+
+<?php get_template_part('templates/page', 'header'); ?>
+
+<div class="container-full home-hero">
+	<div class="container col-sm-12 row">
+		<div class="col-sm-8 col-sm-offset-2">
+			<h1></h1>
+			<p>
+			</p>
+			<button class="btn btn-primary">Contact Us</button>
+		</div>
+	</div>
+</div>
+
+<div class="container-full home-services">
+	<div class="container">
+		<div class="col-sm-4">
+			<h3></h3>
+			<p>
+				
+			</p>
+		</div>
+		<div class="col-sm-4">
+			<h3></h3>
+			<p>
+				
+			</p>
+		</div>
+		<div class="col-sm-4">
+			<h3></h3>
+			<p>
+				
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="container-full home-usp">
+	<div class="container">
+		<div class="col-sm-8 col-sm-offset-2">
+			<h2></h2>
+			<p>
+				
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="container-full home-team">
+	<div class="container">
+		<div class="col-sm-6">
+			<h3></h3>
+			<p>
+				
+			</p>
+		</div>
+		<div class="col-sm-6">
+			<p>
+				
+			</p>
+		</div>
+	</div>
+</div>
+
+<div class="container-full home-contact">
+	<div class="container">
+		<div class="col-sm-6">
+		</div>
+		<div class="col-sm-6">
+		</div>
+	</div>
+</div>
