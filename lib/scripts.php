a245424e7dd099e49af56079351f1321b6d5f1bd
collapsedwave <josh@collapsedwave.com>
2014-03-17 17:59:30 -0400

    layout update 4

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'baabaec487576f1a880096c1f9767c96');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '1ffcd26c109317ba94f7cc8b4b9cd2a7');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
