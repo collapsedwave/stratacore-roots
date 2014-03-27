fb141ce43e2ddeedc7296039144095280ff1bcac
collapsedwave <josh@collapsedwave.com>
2014-03-27 11:37:57 -0400

    correct attribution styling 2

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c96702927e858ea8d156d5e079b28b58');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '59db5597705c8236cdc49669f0d19296');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
